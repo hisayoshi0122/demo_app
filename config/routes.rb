ed1c5909917c072abb83cb4dd35e37a5622857a0
吉田尚央 <hisaology@gmail.com>
2015-09-07 04:47:45 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 DemoApp::Application.routes.draw do
+  get "info/index"
   resources :users
+
+  root :to => 'info#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root :to => 'Users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
